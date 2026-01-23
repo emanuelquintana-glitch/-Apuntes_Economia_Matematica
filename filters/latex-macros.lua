5153565d961b7aae442e76ccc6cf47af0d7d42a6
emanuelquintana-glitch <emanuel.quintana@uptc.edu.co>
2026-01-23 05:38:36 +0000

    Fix: Resolución de conflictos y sincronización con main

---
+++ filters/latex-macros.lua
@@ -0,0 +1,10 @@
+﻿-- Filtro personalizado para macros LaTeX
+function Math(elem)
+    -- Procesar ecuaciones matemáticas
+    return elem
+end
+
+function Pandoc(doc)
+    -- Procesamiento general del documento
+    return doc
+end
